d66ca03b45d78578b946a9460b8e6cf04a60ec36
Salvatore Marragony <salvatore.marragony@mail.polimi.it>
2021-05-26 16:28:30 +0200

    feat: graphic implementation of the marble buffer scene

---
+++ src/main/java/it/polimi/ingsw/client/view/GUI/MarbleBufferController.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.client.view.GUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MarbleBufferController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
